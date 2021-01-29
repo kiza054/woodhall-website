ba68afca40703f4396faff074fa5cc58bb13bf63
Kiran Sarda <kiza054@gmail.com>
2021-01-29 15:54:04 +0000

    Initial commit

--- woodhall_website/settings.py
+++ woodhall_website/settings.py
@@ -90,14 +90,15 @@ WSGI_APPLICATION = 'woodhall_website.wsgi.application'
 
 # Media files (Images, PDF, docx, etc)
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 
 # Django Summernote Variables
 

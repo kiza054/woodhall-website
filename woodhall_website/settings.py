f92194837ae1c96554c0f21ff26b615db1dcda5c
Kiran Sarda <kiza054@gmail.com>
2021-01-29 16:46:51 +0000

    Initial commit

--- woodhall_website/settings.py
+++ woodhall_website/settings.py
@@ -178,17 +178,3 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
-
-# Amazon S3 Settings/Variables
-
-#AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESSKEY_ID')
-#AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRETACCESS_KEY')
-#AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGEBUCKET_NAME')
-
-#AWS_S3_FILE_OVERWRITE = False
-#AWS_DEFAULT_ACL = None
-#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-# django_heroku Settings/Variables
-
-django_heroku.settings(locals())

e49b507dda86407341c29cff972b7a6110d30f8f
Kiran Sarda <kiza054@gmail.com>
2021-01-29 16:52:02 +0000

    Initial commit

--- woodhall_website/settings.py
+++ woodhall_website/settings.py
@@ -178,3 +178,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+
+# django_heroku Settings/Variables
+
+django_heroku.settings(locals())

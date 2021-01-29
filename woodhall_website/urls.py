939c91904d2617d135c8bd4905018698d7af70fd
Kiran Sarda <kiza054@gmail.com>
2021-01-29 16:00:05 +0000

    Initial commit

--- woodhall_website/urls.py
+++ woodhall_website/urls.py
@@ -37,7 +37,6 @@ urlpatterns = [
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'), name='password_reset_done'),
     path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'), name='password_reset_confirm'),
     path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'), name='password_reset_complete'),
-    path('sentry-debug/', trigger_error),
 ]
 
 handler403 = main_website.views.error_403_view

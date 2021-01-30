7470c5d5c4c4ac4db4e6310497ee24e7d97f1505
Kiran Sarda <kiza054@gmail.com>
2021-01-30 17:18:48 +0000

    Initial commit

--- woodhall_website/settings.py
+++ woodhall_website/settings.py
@@ -187,7 +187,6 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME') or 'woodhall
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 AWS_DEFAULT_ACL = 'public-read'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 

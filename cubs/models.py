8efe5bd4fce10696359ae1c6b0077a3b681e98b9
Kiran Sarda <kiza054@gmail.com>
2021-02-01 15:49:24 +0000

    Initial commit

--- cubs/models.py
+++ cubs/models.py
@@ -57,3 +57,6 @@ class File(models.Model):
     description = models.CharField(max_length=255, blank=True)
     file = models.FileField(upload_to='cubs/documents/%Y/%B/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name

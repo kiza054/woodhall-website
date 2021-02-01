ba2e370abe8faa32d7f4affb3cd486c0baa5a175
Kiran Sarda <kiza054@gmail.com>
2021-02-01 13:42:00 +0000

    Initial commit

--- woodhall_website/settings.py
+++ woodhall_website/settings.py
@@ -190,6 +190,7 @@ AWS_DEFAULT_ACL = 'public-read'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 DEFAULT_FILE_STORAGE = 'main_website.storages.MediaStorage'
+#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # django_heroku Settings/Variables
 

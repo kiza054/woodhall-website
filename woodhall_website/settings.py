b60fadd67b29d7cc63ea9105b8fafc25ba8c247d
Kiran Sarda <kiza054@gmail.com>
2021-02-01 16:10:36 +0000

    Initial commit

--- woodhall_website/settings.py
+++ woodhall_website/settings.py
@@ -100,6 +100,11 @@ MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Django Summernote Variables

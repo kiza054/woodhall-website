633327eab20cc408d586c2d1e3dbd4fabf03ccd7
Kiran Sarda <kiza054@gmail.com>
2022-02-16 04:22:35 +0000

    added more fontawesome icons

--- cubs/models.py
+++ cubs/models.py
@@ -36,7 +36,7 @@ class Post(models.Model):
 
 class PostImage(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='cubs_blog_post_images')
-    image = models.ImageField(upload_to='media/post_images/cubs/%Y/%B/', blank=True, null=True)
+    image = models.ImageField(upload_to='media/post_images/cubs/%Y/%B/', blank=True, null=True) # When in development remove 'media/' in upload_to
 
     def __str__(self):
         return self.post.title
@@ -58,7 +58,7 @@ class Comment(models.Model):
 class File(models.Model):
     name = models.CharField(max_length=25, blank=True)
     description = models.CharField(max_length=255, blank=True)
-    file = models.FileField(upload_to='media/cubs/documents/%Y/%B/')
+    file = models.FileField(upload_to='media/cubs/documents/%Y/%B/') # When in development remove 'media/' in upload_to
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):

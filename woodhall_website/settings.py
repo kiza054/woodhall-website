3b8c3c41a9fa75d6ae932b9a85a8088e63d8df27
kiza054 <kiza054@gmail.com>
2022-11-16 16:49:49 +0000

    django celery tests

--- woodhall_website/settings.py
+++ woodhall_website/settings.py
@@ -252,7 +252,7 @@ django_heroku.settings(locals())
 # Django Celery Settings
 
 CELERY_BROKER_URL = "amqps://jspgishl:KwoQhOlJ90BKorrNKZZii8gkzWL6bzmk@whale.rmq.cloudamqp.com/jspgishl" # "redis://localhost:6379"
-CELERY_RESULT_BACKEND = "redis://localhost:6379"
+CELERY_RESULT_BACKEND = "amqps://jspgishl:KwoQhOlJ90BKorrNKZZii8gkzWL6bzmk@whale.rmq.cloudamqp.com/jspgishl"
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'

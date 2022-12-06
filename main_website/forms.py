e3541a190ff2482024584e5b16de313e07837545
kiza054 <kiza054@gmail.com>
2022-12-06 06:16:38 +0000

    changes to forms

--- main_website/forms.py
+++ main_website/forms.py
@@ -154,12 +154,30 @@ class UploadImageForm(forms.ModelForm):
     class Meta:
         model = ImageGallery
         fields = ('category', 'file_name', 'image', 'description')
-        
+    
     def __init__(self, *args, **kwargs):
         super(UploadImageForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+
+        self.helper.layout = Layout(
+            PrependedText('category', 'Category:'),
+            PrependedText('file_name', 'File Name:'),
+            'image',
+            PrependedText('description', 'Description:'),
+            FormActions(
+                HTML('<button class="btn btn-outline-info" type="submit">'
+                    '<i class="fa-duotone fa-file-arrow-up"></i> Upload Images'
+                    '</button>')
+                )
+            )
+
         self.fields['description'].widget.attrs['rows'] = 4
         self.fields['description'].widget.attrs['cols'] = 40
         self.fields['description'].widget.attrs['columns'] = 15
+        self.fields['category'].label = ''
+        self.fields['file_name'].label = ''
+        self.fields['image'].label = ''
+        self.fields['description'].label = ''
 
 class AddImageCategoryForm(forms.ModelForm):
     class Meta:

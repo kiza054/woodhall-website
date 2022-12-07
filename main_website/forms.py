faaa8030bfb22e4ae66132b89b68c91cfa95e6f0
kiza054 <kiza054@gmail.com>
2022-12-07 03:45:39 +0000

    changes to forms

--- main_website/forms.py
+++ main_website/forms.py
@@ -170,7 +170,6 @@ class UploadImageForm(forms.ModelForm):
                     '</button>')
                 )
             )
-
         self.fields['description'].widget.attrs['rows'] = 4
         self.fields['description'].widget.attrs['cols'] = 40
         self.fields['description'].widget.attrs['columns'] = 15
@@ -180,6 +179,22 @@ class UploadImageForm(forms.ModelForm):
         self.fields['description'].label = ''
 
 class AddImageCategoryForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(AddImageCategoryForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+
+        self.helper.layout = Layout(
+            PrependedText('user', 'User:'),
+            PrependedText('name', 'Name:'),
+            FormActions(
+                HTML('<button class="btn btn-outline-info" type="submit">'
+                    '<i class="fa-duotone fa-plus"></i> Add Category'
+                    '</button>')
+                )
+            )
+        self.fields['user'].label = ''
+        self.fields['name'].label = ''
+
     class Meta:
         model = ImageGalleryCategory
         fields = ('user', 'name')
